aa9337b743c549e7a3fd1c8fa575dacd59e7294a
Vince Schwidder <vince@yummygum.com>
2026-08-04 12:58:15 +0200

    Update and rename config.js to js/config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  GOOGLE_CLIENT_ID: "VUL-HIER-JE-CLIENT-ID-IN.apps.googleusercontent.com",
-  API_URL: "VUL-HIER-JE-APPS-SCRIPT-WEB-APP-URL-IN",
-  APP_NAME: "Orders",
-};
---
+++ js/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  GOOGLE_CLIENT_ID: "538904702384-t7e5ofk5f6iqu91afild7m7o2pu60vv9.apps.googleusercontent.com",
+  API_URL: "1Ei2DZ3QddN3G_b8KyoiE5s6N3rwvdtx1EJRk9gvemxo",
+  APP_NAME: "Orders",
+};
