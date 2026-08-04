f74bd89b0b49a67e759fa23c0f7870bba147a60f
vschwid <vince@yummygum.com>
2026-08-04 17:27:12 +0200

    Config fix

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GOOGLE_CLIENT_ID: "VUL-HIER-JE-CLIENT-ID-IN.apps.googleusercontent.com",
-  API_URL: "VUL-HIER-JE-APPS-SCRIPT-WEB-APP-URL-IN",
-  APP_NAME: "Orders",
+  GOOGLE_CLIENT_ID: "538904702384-t7e5ofk5f6iqu91afild7m7o2pu60vv9.apps.googleusercontent.com",
+  API_URL: "https://script.google.com/macros/s/AKfycby5edLFj-WFBV7QLr8UY2x2HOJX8IagUJ2rJEWDLAb3TbEV_MYIHFAKeRWVCLfh34-WVA/exec",
+  APP_NAME: "StickOrders",
 };
