8c41a216401be719b3d24863c728292ce1b82ff4
Vince Schwidder <vince@yummygum.com>
2026-08-04 13:02:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   GOOGLE_CLIENT_ID: "538904702384-t7e5ofk5f6iqu91afild7m7o2pu60vv9.apps.googleusercontent.com",
-  API_URL: "1Ei2DZ3QddN3G_b8KyoiE5s6N3rwvdtx1EJRk9gvemxo",
-  APP_NAME: "Orders",
+  API_URL: "https://script.google.com/macros/s/1Ei2DZ3QddN3G_b8KyoiE5s6N3rwvdtx1EJRk9gvemxo",
+  APP_NAME: "StickOrders",
 };
